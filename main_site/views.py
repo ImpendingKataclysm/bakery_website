985f6775f2fa0ed0ff394c7c354eb85990657508
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-04-28 20:53:12 -0700

    Created an 'About' page and linked to it from the nav menu and the site map

--- main_site/views.py
+++ main_site/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     """
-    :param request: HTTP request to render specified content
-    :return: HTTP response containing request result
+    :param request: HTTP request to render the home page
+    :return: HTTP response used to render the home page in the browser
     """
     return render(request, "index.html")
+
+
+def about(request):
+    """
+    :param request: HTTP request to render the about page
+    :return: HTTP response used to render the about page in the browser
+    """
+    return render(request, "about.html")
