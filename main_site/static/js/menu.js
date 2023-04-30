359cda639198f4f7bd78264f95874cd3fd81f98b
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-04-30 16:15:09 -0700

    Toggle the side nav display on the products page with the categories button

--- main_site/static/js/menu.js
+++ main_site/static/js/menu.js
@@ -1,9 +1,30 @@
-const nav = document.querySelector("nav");
+const nav = document.getElementById("main_nav");
+const side_nav = document.getElementById("side_nav")
 const nav_btn = document.getElementById("nav-btn");
+const side_nav_btn = document.getElementById("side_nav_btn");
 
 nav_btn.addEventListener("click", showNav);
+
+if(side_nav_btn)
+{
+    side_nav_btn.addEventListener("click", showSideNav);
+}
 
 function showNav()
 {
     nav.classList.toggle("show");
+}
+
+function showSideNav()
+{
+    side_nav.classList.toggle("show");
+
+    if(side_nav_btn.textContent.includes('See'))
+    {
+        side_nav_btn.textContent = 'Hide Categories';
+    }
+    else if(side_nav_btn.textContent.includes('Hide'))
+    {
+        side_nav_btn.textContent = 'See Categories';
+    }
 }
