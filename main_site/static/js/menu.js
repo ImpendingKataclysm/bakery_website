60662b6f69ed02c27af5e778183cf08dcd92b0a9
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-04-30 16:31:35 -0700

    Improved overall page layouts and fixed the sidenav on the products page so it will automatically hide when one of its items is clicked

--- main_site/static/js/menu.js
+++ main_site/static/js/menu.js
@@ -2,12 +2,21 @@ const nav = document.getElementById("main_nav");
 const side_nav = document.getElementById("side_nav")
 const nav_btn = document.getElementById("nav-btn");
 const side_nav_btn = document.getElementById("side_nav_btn");
+const side_nav_items = document.getElementsByClassName("sidenav-item");
 
 nav_btn.addEventListener("click", showNav);
 
 if(side_nav_btn)
 {
     side_nav_btn.addEventListener("click", showSideNav);
+}
+
+if(side_nav_items)
+{
+    Array.from(side_nav_items).forEach(function (e)
+    {
+        e.addEventListener("click", showSideNav)
+    })
 }
 
 function showNav()
